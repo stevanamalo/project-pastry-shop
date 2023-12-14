eeeea0243277ed406ae62dd1e92a622873175ec9
stevanamalo <estifansa@gmail.com>
2023-12-15 01:08:21 +0700

    updatemastermenu

--- resources/views/baker/mastermenu.blade.php
+++ resources/views/baker/mastermenu.blade.php
@@ -5,10 +5,9 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Master Menu</title>
+  <title>Register karyawan</title>
 
   <!-- Add your styles here -->
-  <!-- (Styles from the previous HTML file) -->
   <style>
     body {
       font-family: 'Open Sans', sans-serif;
@@ -27,6 +26,14 @@
     .container {
       margin: 20px auto;
       max-width: 400px;
+      background-color: white;
+      padding: 20px;
+      border-radius: 8px;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    }
+    .container1 {
+      margin: 20px auto;
+      max-width: 170px;
       background-color: white;
       padding: 20px;
       border-radius: 8px;
@@ -64,13 +71,15 @@
   </style>
 </head>
 
+
 <body>
+
   <header>
     <h1>Master Menu</h1>
   </header>
 
   <div class="container">
-    <h2>Add New Pastry</h2>
+    <h2>Add New Menu</h2>
 
     @if(session('msg'))
     <div class="alert">
@@ -88,37 +97,41 @@
       </div>
     @endif
 
-    <form method="post" action="{{ url("/baker/insertpastry") }}">
-      @csrf
+    <form method="post" action="{{ url("/baker/insertpastry") }}" enctype="multipart/form-data">
+        @csrf
+        <label for="picturepastry">Pastry Picture</label>
+        <input type="file" name="picturepastry" accept="image/*" required>
+        <label for="nama">Pastry Name</label>
+        <input type="text" name="nama" required>
+        <label for="harga">Price</label>
+        <input type="number" name="harga" required>
+        <label for="ingredients_id">Ingredients</label>
+        <select name="ingredients_id" required>
+           <option value="" disabled selected>Select Ingredients</option>
+           @foreach ($ingredients as $ingredient)
+              <option value="{{ $ingredient->id }}">{{ $ingredient->nama }}</option>
+           @endforeach
+        </select>
+        <br><br>
+        <button type="submit">Add Pastry</button>
+        <br><br>
+        <a href="{{ url('/baker')  }}">Back to Baker</a>
+     </form>
 
-      <label for="nama">Pastry Name</label>
-      <input type="text" name="nama" required>
-
-      <label for="harga">Price</label>
-      <input type="number" name="harga" required>
-
-      <label for="ingredients_id">Ingredients</label>
-      <select name="ingredients_id" required>
-        <option value="" disabled selected>Select Ingredients</option>
-        @foreach ($ingredients as $ingredient)
-          <option value="{{ $ingredient->id }}">{{ $ingredient->nama }}</option>
-        @endforeach
-    </select>
-
-      <button type="submit">Add Pastry</button>
-
-      <br>
-
-      <a href="{{ url('/baker')  }}">Back to Baker</a>
-    </form>
-
-    <h2>Existing Pastries</h2>
-
+    <h2>List Pastries</h2>
     <ul>
-      @foreach ($pastries as $pastry)
-        <li>{{ $pastry->nama }} - Price: {{ $pastry->harga }} - Ingredients: {{ $pastry->ingredient->nama }}</li>
-      @endforeach
-    </ul>
+        @foreach ($pastries as $pastry)
+            <div class="container1">
+                <center>
+                    <img src="{{ asset($pastry->picturepastry) }}" alt="{{ $pastry->nama }}" width="120">
+                </center>
+              <br>
+              {{ $pastry->nama }}
+              <br>Price: {{ $pastry->harga }}
+              <br>Ingredients: {{ $pastry->ingredient->nama }}
+            </div>
+        @endforeach
+     </ul>
   </div>
 
 </body>
